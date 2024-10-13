39589598d2bdbb11ab2c4f67b00e93d9d2696b63
wenyang <131729375+wenyang015@users.noreply.github.com>
2024-10-13 21:47:24 +0800

    update files

---
+++ python/python杂谈/迭代和生成之辩/单词序列.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+"""
+
+PyCharm 单词序列.py
+2024年10月13日21时22分
+by wenyang
+
+"""
+# 单词序列
+class Sentence(object):
+
+    def __init__(self, text):
+        self.text = text
+        self.words = self.text.split(" ")
+
+    def __len__(self):
+        return len(self.words)
+
+    def __getitem__(self, position):
+        return self.words[position]
+
+    def __repr__(self):
+        return f"Sentence({self.text})"
+
+def main():
+    s = Sentence("hello world javascript python 3.12 what up")
+    for word in s.words:
+        print(word)
+    print(s)
+    print(list(s))
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/python杂谈/迭代和生成之辩/迭代器抛出typeerror异常.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+"""
+
+PyCharm 迭代器抛出typeerror异常.py
+2024年10月13日21时38分
+by wenyang
+
+"""
+
+a = 10
+
+def main():
+    # for i in a: # 此时就是会抛出 'int' object is not iterable的异常报告
+    #     pass
+    ...
+
+if __name__ == "__main__":
+    main()
