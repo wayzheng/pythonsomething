4cc285a6e0065409ef34498f830c802c7180fac9
wenyang <131729375+wenyang015@users.noreply.github.com>
2024-10-17 09:43:51 +0800

    docs & files:(python函数进阶)添加文档内容和完善代码

--- python/python进阶/python函数进阶/装饰器函数.py
+++ python/python进阶/python函数进阶/装饰器函数.py
@@ -27,13 +27,32 @@ def outer1(times):
                 func(*args, **kwargs)
         return wrapper
     return outer1
-
+# 通过一个装饰器函数来实现计算函数执行的次数的功能
+def outer2(func):
+    """
+    通过一个装饰器函数来实现计算函数执行的次数的功能
+    :param func: 就是要被装饰的函数
+    :return: 返回的就是一个函数对象
+    """
+    count = 0
+    def wrapper(*args, **kwargs):
+        nonlocal count
+        count += 1
+        func(*args, **kwargs)
+        print(f"函数执行了{count}次")
+    return wrapper
 @outer1(3)
 def func1():
     print("*" * 10)
+# 装饰器如果没有参数要进行传递的时候 就是不需要在()内部加上参数
+@outer2
+def bar(message):
+    print("hello world" + message)
 def main():
     func()
     func1()
+    bar("javascript")
+    bar("javascript")
 
 
 if __name__ == "__main__":
